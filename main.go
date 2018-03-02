764ce23459d5027f907ea8f69c8cc1abcac1948c
Jiajun Huang <gansteed@gmail.com>
2018-03-02 21:13:29 +0800

    initial version

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
 
+func main() {
+	cmd := exec.Command("/bin/bash")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER,
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("failed to run command: %s", err)
+	}
 }
