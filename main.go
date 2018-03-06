712e3e6fef62206d6716f3718c57b9c2a0dfaca2
Jiajun Huang <gansteed@gmail.com>
2018-03-06 21:22:26 +0800

    uid & gid mapping

--- main.go
+++ main.go
@@ -14,8 +14,20 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	dirpath, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("failed to get current work directory: %s", err)
+	}
+
 	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Chroot:     dirpath + "./container_root",
 		Cloneflags: syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			syscall.SysProcIDMap{ContainerID: 0, HostID: os.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			syscall.SysProcIDMap{ContainerID: 0, HostID: os.Getgid(), Size: 1},
+		},
 	}
 
 	if err := cmd.Run(); err != nil {
