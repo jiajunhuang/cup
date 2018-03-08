d52bda25f8d4448b967ba672611316176b694f0e
Jiajun Huang <gansteed@gmail.com>
2018-03-08 20:46:55 +0800

    use reexec

--- main.go
+++ main.go
@@ -3,24 +3,40 @@ package main
 import (
 	"log"
 	"os"
-	"os/exec"
 	"syscall"
+
+	"github.com/docker/docker/pkg/reexec"
 )
 
+func init() {
+	reexec.Register("childProcess", childProcess)
+	if reexec.Init() {
+		os.Exit(0)
+	}
+}
+
+func childProcess() {
+	log.Printf("childProcess start...uid: %d, gid: %d\n", os.Getuid(), os.Getgid())
+	if h, err := os.Hostname(); err == nil {
+		log.Printf("child: hostname: %s\n", h)
+	}
+	if err := syscall.Sethostname([]byte("cup-host")); err != nil {
+		log.Panicf("failed to set hostname: %s", err)
+	}
+	if h, err := os.Hostname(); err == nil {
+		log.Printf("child: hostname: %s\n", h)
+	}
+}
+
 func main() {
-	cmd := exec.Command("/bin/bash")
+	log.Println("main start...")
+	cmd := reexec.Command("childProcess")
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	dirpath, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("failed to get current work directory: %s", err)
-	}
-
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Chroot:     dirpath + "./container_root",
 		Cloneflags: syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER,
 		UidMappings: []syscall.SysProcIDMap{
 			syscall.SysProcIDMap{ContainerID: 0, HostID: os.Getuid(), Size: 1},
@@ -32,5 +48,8 @@ func main() {
 
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("failed to run command: %s", err)
+	}
+	if h, err := os.Hostname(); err == nil {
+		log.Printf("main: hostname: %s\n", h)
 	}
 }
